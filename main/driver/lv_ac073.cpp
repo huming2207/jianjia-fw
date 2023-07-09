25c2d0df4108546b2bc6a8daaeec7efc2aabfbe7
Jackson Ming Hu <huming2207@gmail.com>
2023-07-09 18:33:48 +1000

    lv_ac073: init

--- main/driver/lv_ac073.cpp
+++ main/driver/lv_ac073.cpp
@@ -1,5 +1,58 @@
-//
-// Created by hu on 9/07/23.
-//
+#include <lvgl.h>
+#include <esp_log.h>
 
 #include "lv_ac073.hpp"
+
+uint8_t lv_ac073::lv_fb[ac073_def::hor_size * ac073_def::ver_size] = {};
+uint8_t lv_ac073::disp_fb[ac073_def::hor_size * ac073_def::ver_size / 2] = {};
+
+esp_err_t lv_ac073::init(gpio_num_t _sda, gpio_num_t _scl, gpio_num_t _cs, gpio_num_t _dc, gpio_num_t _rst, gpio_num_t _busy, spi_host_device_t _spi_periph)
+{
+    auto ret = epd->init(_sda, _scl, _cs, _dc, _rst, _busy, _spi_periph);
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    lv_init();
+    lv_disp_draw_buf_init(&lv_draw_buf, lv_fb, nullptr, ac073_def::hor_size * ac073_def::ver_size);
+
+    lv_disp_drv_t disp_drv = {};
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = flush_handler;
+    disp_drv.antialiasing = 0;
+    disp_drv.full_refresh = 1;
+    disp_drv.draw_buf = &lv_draw_buf;
+    disp_drv.hor_res = ac073_def::hor_size;
+    disp_drv.ver_res = ac073_def::ver_size;
+    disp_drv.user_data = this;
+    lv_disp = lv_disp_drv_register(&disp_drv);
+    if (lv_disp == nullptr) {
+        ESP_LOGE(TAG, "LVGL init seems failed??");
+        return ESP_FAIL;
+    }
+
+
+    return ret;
+}
+
+void lv_ac073::flush_handler(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
+{
+    auto *ctx = (lv_ac073 *)disp_drv->user_data;
+
+    size_t buf_len = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1);
+    if (buf_len < (ac073_def::ver_size * ac073_def::hor_size)) {
+        ESP_LOGE(TAG, "Framebuffer is not full, x1=%d, y1=%d; x2=%d, y2=%d", area->x1, area->y1, area->x2, area->y2);
+        return;
+    }
+
+    size_t processed_pix_cnt = 0;
+    for (size_t idx = 0; idx < buf_len; idx += 2) {
+        if (processed_pix_cnt >= sizeof(disp_fb)) {
+            ESP_LOGE(TAG, "Out of bound!");
+            break;
+        }
+
+        disp_fb[processed_pix_cnt] = (de_palette(color_p[idx]) << 4) | (de_palette(color_p[idx + 1]));
+        processed_pix_cnt += 1;
+    }
+}
--- main/driver/lv_ac073.hpp
+++ main/driver/lv_ac073.hpp
@@ -1,15 +1,132 @@
-//
-// Created by hu on 9/07/23.
-//
+#pragma once
 
-#ifndef MAIN_LV_AC073_HPP
-#define MAIN_LV_AC073_HPP
+#include <cstdint>
+#include <esp_err.h>
+
+#include "ac073tc1.hpp"
+
+namespace ac073_def
+{
+    // From Waveshare demo code
+    static const constexpr uint8_t palette[8 * 3] = {
+            0, 0, 0,
+            255, 255, 255,
+            67, 138, 28,
+            100, 64, 255,
+            191, 0, 0,
+            255, 243, 56,
+            232, 126, 0,
+            194 ,164 , 244 // Why we need this line (21-23)??
+    };
+
+    enum colors : uint8_t {
+        BLACK = 0,
+        WHITE = 1,
+        GREEN = 2,
+        BLUE = 3,
+        RED = 4,
+        YELLOW = 5,
+        ORANGE = 6,
+    };
+
+    static const constexpr uint8_t palette_lut[] = {
+            BLACK, BLUE, BLUE, BLUE, // 0x00
+            BLACK, BLUE, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE, // 0x10
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            BLACK, BLUE, BLUE, BLUE, // 0x20
+            BLACK, BLUE, BLUE, BLUE,
+            GREEN, BLUE, BLUE, BLUE,
+            GREEN, BLUE, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE, // 0x30
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, GREEN, BLUE,
+            BLACK, BLUE, BLUE, BLUE, // 0x40
+            RED, BLUE, BLUE, BLUE,
+            GREEN, BLUE, BLUE, BLUE,
+            GREEN, BLUE, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE, // 0x50
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, GREEN, BLUE,
+            GREEN, GREEN, GREEN, BLUE,
+            RED, BLUE, BLUE, BLUE, // 0x60
+            RED, BLUE, BLUE, BLUE,
+            ORANGE, BLUE, BLUE, BLUE,
+            YELLOW, BLUE, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE, // 0x70
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            RED, BLUE, BLUE, BLUE, // 0x80
+            RED, BLUE, BLUE, BLUE,
+            ORANGE, BLUE, BLUE, BLUE,
+            ORANGE, BLUE, BLUE, BLUE,
+            ORANGE, GREEN, BLUE, BLUE, // 0x90
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            GREEN, GREEN, BLUE, BLUE,
+            RED, BLUE, BLUE, BLUE, // 0xa0
+            RED, BLUE, BLUE, BLUE,
+            ORANGE, ORANGE, BLUE, BLUE,
+            YELLOW, YELLOW, BLUE, BLUE,
+            YELLOW, YELLOW, BLUE, BLUE, // 0xb0
+            YELLOW, YELLOW, BLUE, BLUE,
+            YELLOW, GREEN, BLUE, BLUE,
+            YELLOW, GREEN, BLUE, BLUE,
+            RED, BLUE, BLUE, BLUE, // 0xc0
+            RED, RED, BLUE, BLUE,
+            ORANGE, RED, BLUE, BLUE,
+            ORANGE, ORANGE, BLUE, BLUE,
+            YELLOW, ORANGE, BLUE, BLUE, // 0xd0
+            YELLOW, YELLOW, BLUE, BLUE,
+            YELLOW, YELLOW, WHITE, BLUE,
+            YELLOW, YELLOW, WHITE, WHITE,
+            RED, RED, BLUE, BLUE, // 0xe0
+            RED, RED, BLUE, BLUE,
+            ORANGE, RED, BLUE, BLUE,
+            ORANGE, ORANGE, BLUE, BLUE,
+            ORANGE, YELLOW, BLUE, BLUE, // 0xf0
+            ORANGE, YELLOW, WHITE, WHITE,
+            ORANGE, YELLOW, WHITE, WHITE,
+            YELLOW, YELLOW, WHITE, WHITE,
+    };
+}
+
 
 
 class lv_ac073
 {
+public:
+    static lv_ac073 *instance()
+    {
+        static lv_ac073 _instance;
+        return &_instance;
+    }
 
+    lv_ac073(lv_ac073 const &) = delete;
+    void operator=(lv_ac073 const &) = delete;
+
+public:
+    esp_err_t init(gpio_num_t _sda = GPIO_NUM_16, gpio_num_t _scl = GPIO_NUM_15, gpio_num_t _cs = GPIO_NUM_7,
+                   gpio_num_t _dc = GPIO_NUM_6, gpio_num_t _rst = GPIO_NUM_5, gpio_num_t _busy = GPIO_NUM_4, spi_host_device_t _spi_periph = SPI3_HOST);
+
+private:
+    lv_ac073() = default;
+    static void flush_handler(lv_disp_drv_t *disp_drv, const lv_area_t * area, lv_color_t * color_p);
+    static inline uint8_t de_palette(lv_color_t color);
+
+private:
+    static const constexpr char TAG[] = "lv_ac073";
+    static uint8_t lv_fb[ac073_def::hor_size * ac073_def::ver_size];
+    static uint8_t disp_fb[ac073_def::hor_size * ac073_def::ver_size / 2];
+
+private:
+    ac073tc1 *epd = ac073tc1::instance();
+    lv_disp_draw_buf_t lv_draw_buf = {};
+    lv_disp_t *lv_disp = nullptr;
 };
-
-
-#endif //MAIN_LV_AC073_HPP
